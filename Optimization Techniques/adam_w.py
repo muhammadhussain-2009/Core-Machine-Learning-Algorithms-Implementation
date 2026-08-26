ef3da746ce4d21a272ceeeb89b876f2aa3358a90
Muhammad Hussain <mhussain.20094@gmail.com>
2026-08-26 15:50:03 +0500

    AdamW (Decoupled Weight Decay) optimization
    Implement AdamW optimization step function.

---
+++ Optimization Techniques/adam_w.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def adamw_step(w, m, v, grad, lr=0.001, beta1=0.9, beta2=0.999, weight_decay=0.01, eps=1e-8):
+    """
+    Perform one AdamW update step.
+    """
+    # Write code here
+    w = np.array(w)
+    m = np.array(m)
+    v = np.array(v)
+    grad = np.array(grad)
+    m_new = beta1 * m + (1 - beta1) * grad
+    v_new = beta2 * v + (1 - beta2) * (grad**2)
+    w_new = w - (lr * weight_decay* w) - (lr * (m_new / (np.sqrt(v_new) + eps)))
+    
+    return w_new, m_new, v_new
+    pass
